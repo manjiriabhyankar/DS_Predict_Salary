eb6130147050fcbc6b3fd9a35231ede7842ff853
Manjiri <manjiria95@gmail.com>
2020-09-01 23:19:51 +0530

    adding files

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,8 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 31 23:09:18 2020
+import pandas as pd
 
-@author: admin
-"""
+df = pd.read_csv('glassdoor_jobs.csv')     
+
+
+#removing all rows with salary estimate as unknown
+df = df[df['Salary Estimate']!='-1']
+
+#removing unnecessary keywords in salary estimates
+salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
+
+remove_K=salary.apply(lambda x: x.replace('K','').replace('$',''))
+
+df[min_salary] = remove_K.apply(lambda x:x.split('-')[0])
+
+df[max_salary] = remove_K.apply(lambda x:x.split('-')[1])
+
 
 
